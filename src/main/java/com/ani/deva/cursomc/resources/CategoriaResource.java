1ebd5e692a31e8c82f8678470b8390f6b37aad09
Anelisi <anelisilanmeller@gmail.com>
2021-03-04 17:34:29 -0300

    REST testado

---
+++ src/main/java/com/ani/deva/cursomc/resources/CategoriaResource.java
@@ -0,0 +1,15 @@
+package com.ani.deva.cursomc.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categorias")
+public class CategoriaResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String listar() {
+        return  "Parabéns, Anelisi! REST está funcionando!";
+    }
+}
